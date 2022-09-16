04faac5fd901b2723e5f49d93c701fbe77a90568
Antoni Keszycki <a.keszycki@gmail.com>
2022-09-16 21:59:26 +0200

    redesign edit page

--- models/campground.js
+++ models/campground.js
@@ -11,6 +11,10 @@ ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
+ImageSchema.virtual("preview").get(function () {
+  return this.url.replace("/upload", "/upload/w_100");
+});
+
 const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema(

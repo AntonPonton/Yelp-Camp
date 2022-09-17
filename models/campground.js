c3bde4dc83c282449540fd9a92bc2bf0695388c8
Antoni Keszycki <a.keszycki@gmail.com>
2022-09-17 16:17:00 +0200

    fix 'no image' bug

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { campgroundSchema } = require("../schemas");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
@@ -53,8 +54,51 @@ const CampgroundSchema = new Schema(
   opts
 );
 
-/* `<img src="${this.images[0].url}" class="pop-up-image"/>
-<a href="/campgrounds/${this._id}" class="link-dark">${this.title}</a>*/
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  let result;
+  if (this.images.length !== 0) {
+    result = `<div class="card text-center border-0" style="width: 10rem;">
+<a href="/campgrounds/${this._id}" class="link-dark">
+<img src="${this.images[0].url}" class="pop-up-image"/>
+</a>
+<p class="card-title mt-2"><strong>${this.title}</strong></p>
+<p class="card-title"><small>${this.price} CHF / per night</small></p>
+</div>`;
+  } else {
+    result = `<div class="card text-center border-0" style="width: 10rem;">
+    <div
+    class="bg-light card-img-top square img-fluid d-flex flex-column align-items-center justify-content-center"
+    >
+    <a href="/campgrounds/${this._id}" class="link-dark">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="66"
+            height="66"
+            fill="gray"
+            class="bi bi-emoji-frown"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+            />
+            <path
+              d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"
+            />
+          </svg>
+          </a>
+        <p class="text-muted mt-3">No photos yet!</p>
+      </div>
+<p class="card-title mt-2"><strong>${this.title}</strong></p>
+<p class="card-title"><small>${this.price} CHF / per night</small></p>
+</div>`;
+  }
+  return result;
+});
+
+/*
+
+
+
 CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
   return `
 <div class="card text-center border-0" style="width: 10rem;">
@@ -66,7 +110,7 @@ CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
 </div>
 `;
 });
-
+*/
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
   if (doc) {
     await Review.deleteMany({

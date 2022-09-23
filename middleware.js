672456fe23387453d6b88afe60debae40c3f8a26
Antoni Keszycki <a.keszycki@gmail.com>
2022-09-23 13:45:33 +0200

    deploy

--- middleware.js
+++ middleware.js
@@ -24,7 +24,9 @@ module.exports.validateCampground = (req, res, next) => {
 
 module.exports.isAuthor = async (req, res, next) => {
   const campground = await Campground.findById(req.params.id);
-  if (!campground.author.equals(req.user._id)) {
+  if (!campground.author.equals(req.user._id) && !req.user.admin) {
+    console.log(!req.user.admin);
+    console.log(!campground.author.equals(req.user._id));
     req.flash("error", "You do not have the permission!");
     return res.redirect(`/campgrounds/${req.params.id}`);
   }
